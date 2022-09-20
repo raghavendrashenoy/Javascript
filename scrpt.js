8d9cd51ff10e4977a9f5061b0bbd4bb7d033405a
raghavendrashenoy <rahul515.shenoy@gmail.com>
2022-09-20 18:56:20 +0530

    thing added

---
+++ scrpt.js
@@ -0,0 +1,23 @@
+let js = ("amazing");
+        if(js === "iji")
+        {
+            alert("js is amazing");
+        } 
+        else{
+          alert("js is bullshit");
+        }
+        console.log(45+90*90);
+
+
+
+        let country = "india";
+        let continent = "asia";
+        let population = "102 crores";
+        console.log(country);
+        console.log(continent);
+        console.log(population);
+
+        
+
+
+                
